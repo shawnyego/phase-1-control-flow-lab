5dd296c7e2ce9f4be3c91c6aeaab11c05e3ef805
shawn <parvonisyego@gmail.com>
2023-03-15 14:05:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  // Write your code here!
+if (feet<=400){
+  return('This one is on me!')
+}
+if (feet < 2500){
+  return ('I will gladly take your thirty bucks.')
+}
+if (feet > 2500){
+  return('No can do.')
+}
+}
+function ternaryCheckCity(city){
+  switch(city){
+    case "NYC":
+      return 'Ok, sounds good.'
+      break
+    case "Pittsburgh":
+      return 'No go.'
+
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case "generous":
+      return ('Thank you so much.')
+      break
+    case 'not as generous':
+      return('Thank you.')
+      break
+    default:
+      return("Bye.")
+  }
+    
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
 }
